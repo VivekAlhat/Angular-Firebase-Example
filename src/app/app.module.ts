a354267d0229807adfd71a960ff693cbbbe70770
VivekAlhat <vivekalhat14@outlook.com>
2020-03-18 16:38:15 +0530

    Final Working Build

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,14 +10,7 @@ import { LoginComponent } from './login/login.component';
 
 
 let firebaseConfig = {
-  apiKey: "AIzaSyBpK6JYHeQD4QZ-CbctiNoXunwb1Ft_bxU",
-  authDomain: "signup-form-7ca97.firebaseapp.com",
-  databaseURL: "https://signup-form-7ca97.firebaseio.com",
-  projectId: "signup-form-7ca97",
-  storageBucket: "signup-form-7ca97.appspot.com",
-  messagingSenderId: "264206051143",
-  appId: "1:264206051143:web:76e82df0e5cdc1a5898e62",
-  measurementId: "G-JFKVTLPBJX"
+  // Replace with your firebase object
 };
 firebase.initializeApp(firebaseConfig)
 
